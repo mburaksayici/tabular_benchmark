b9fa5488fae98ff24086f8f78cd63a3727c9365d
mehmet burak sayıcı <mehmetburaksayici@Mehmets-MacBook-Air.local>
2023-01-09 01:16:44 +0300

    style(black): style black changes

--- tabular_benchmark/benchmark.py
+++ tabular_benchmark/benchmark.py
@@ -1,33 +1,29 @@
-
-
-import pandas as pd 
-import json 
+import pandas as pd
+import json
 
 data = "data/bank/bank-full.csv"
 
-with open('data/bank/params.json') as json_file:
+with open("data/bank/params.json") as json_file:
     data_params = json.load(json_file)
 
 
+df = pd.read_csv(data, sep=";")
 
-df = pd.read_csv(data, sep = ';')
-
-object_columns_feat = cat_features = df.select_dtypes(include='object').columns.tolist()
-
+object_columns_feat = cat_features = df.select_dtypes(include="object").columns.tolist()
 
 
-def convert_cat_str_to_int(df,cat_features):
+def convert_cat_str_to_int(df, cat_features):
     rule_dict = dict()
     for cat_feat in cat_features:
         uniques_ordered = sorted(df[cat_feat].unique().tolist())
-        rule_dict[cat_feat] = rule = dict(zip(uniques_ordered,range(len(uniques_ordered))))
+        rule_dict[cat_feat] = rule = dict(
+            zip(uniques_ordered, range(len(uniques_ordered)))
+        )
         df[cat_feat] = df[cat_feat].map(rule)
     return df, rule_dict
 
 
-
-
-df, rule_dict = convert_cat_str_to_int(df,cat_features)
+df, rule_dict = convert_cat_str_to_int(df, cat_features)
 
 
 from ml_models.lightgbm.lgbm_model import LGBMModel
--- tabular_benchmark/ml_models/lightgbm/lgbm_model.py
+++ tabular_benchmark/ml_models/lightgbm/lgbm_model.py
@@ -6,6 +6,7 @@ import sklearn.datasets
 import sklearn.metrics
 from sklearn.model_selection import train_test_split
 
+
 class OptunaObjective:
     def __init__(self, problem_type, dtrain, dvalid, categorical_feature):
         # Hold this implementation specific arguments as the fields of the class.
@@ -14,7 +15,7 @@ class OptunaObjective:
         self.dtrain = dtrain
         self.dvalid = dvalid
         self.categorical_feature = categorical_feature
-        
+
     def __call__(self, trial):
         param = {
             "objective": "binary",
@@ -32,16 +33,20 @@ class OptunaObjective:
 
         # Add a callback for pruning.
         pruning_callback = optuna.integration.LightGBMPruningCallback(trial, "auc")
-        gbm = lgb.train(param, self.dtrain, valid_sets=[self.dvalid], callbacks=[pruning_callback], categorical_feature=self.categorical_feature)
+        gbm = lgb.train(
+            param,
+            self.dtrain,
+            valid_sets=[self.dvalid],
+            callbacks=[pruning_callback],
+            categorical_feature=self.categorical_feature,
+        )
         trial.set_user_attr(key="params", value=param)
 
         preds = gbm.predict(valid_x)
-        
+
         pred_labels = np.rint(preds)
         accuracy = sklearn.metrics.accuracy_score(valid_y, pred_labels)
         return accuracy
-
-
 
 
 class LGBMModel:
@@ -52,26 +57,39 @@ class LGBMModel:
 
         categorical_feature = data_params["numerical_columns"]
 
-        data, target = df.loc[:, df.columns != 'y'],  df.loc[:, df.columns == 'y']
-        train_x, valid_x, train_y, valid_y = train_test_split(data, target, test_size=0.25)
+        data, target = df.loc[:, df.columns != "y"], df.loc[:, df.columns == "y"]
+        train_x, valid_x, train_y, valid_y = train_test_split(
+            data, target, test_size=0.25
+        )
 
-        dtrain = lgb.Dataset(train_x, label=train_y,   categorical_feature=object_columns_feat)
-        dvalid = lgb.Dataset(valid_x, label=valid_y,   categorical_feature=object_columns_feat)
-        
-        preprocessed_data = [dtrain,dvalid, categorical_feature]
+        dtrain = lgb.Dataset(
+            train_x, label=train_y, categorical_feature=object_columns_feat
+        )
+        dvalid = lgb.Dataset(
+            valid_x, label=valid_y, categorical_feature=object_columns_feat
+        )
 
-        return preprocessed_data 
+        preprocessed_data = [dtrain, dvalid, categorical_feature]
+
+        return preprocessed_data
 
     def fit_model(self, preprocessed_data, hyperparam_opt=True):
         dtrain = preprocessed_data[0]
         dvalid = preprocessed_data[1]
         categorical_feature = preprocessed_data[2]
 
-        if hyperparam_opt: 
+        if hyperparam_opt:
             study = optuna.create_study(
-                pruner=optuna.pruners.MedianPruner(n_warmup_steps=10), direction="maximize"
+                pruner=optuna.pruners.MedianPruner(n_warmup_steps=10),
+                direction="maximize",
             )
-            study.optimize(OptunaObjective(problem_type="classification", dtrain=dtrain, dvalid=dvalid), categorical_feature = categorical_feature, n_trials=20)
+            study.optimize(
+                OptunaObjective(
+                    problem_type="classification", dtrain=dtrain, dvalid=dvalid
+                ),
+                categorical_feature=categorical_feature,
+                n_trials=20,
+            )
 
             print("Number of finished trials: {}".format(len(study.trials)))
 
